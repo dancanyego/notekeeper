8d48f889ed3b1fdd537d1524b543731359315619
Inventrohyder <haitham.hyder@gmail.com>
2019-06-02 16:13:25 -0700

    Populating a spinner

--- app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteActivity.java
+++ app/src/main/java/com/inventrohyder/pluralsight_notekeeper/NoteActivity.java
@@ -8,6 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -18,14 +23,15 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Spinner spinnerCourses = findViewById(R.id.spinner_courses);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        ArrayAdapter<CourseInfo> adapterCourses =
+                new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerCourses.setAdapter(adapterCourses);
+
     }
 
     @Override
